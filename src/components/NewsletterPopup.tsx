2171b60a0d354ccf08b68e673a655a3503f4e4d0
Emma Millereau <emmamillereau@MacBook-Pro-de-Emma-2.local>
2026-09-09 11:15:48 +0200

    Remove duplicate newsletter popup header

--- src/components/NewsletterPopup.tsx
+++ src/components/NewsletterPopup.tsx
@@ -130,7 +130,7 @@ export default function NewsletterPopup() {
             </p>
           </div>
 
-          <NewsletterForm source="popup" submitLabel="Je m’inscris" inputRef={inputRef} tone="light" />
+          <NewsletterForm source="popup" submitLabel="Je m’inscris" inputRef={inputRef} tone="light" hideHeader />
         </div>
       </div>
     </>
